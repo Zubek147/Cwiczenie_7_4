847f426e507913ce9633116365fbf1cf0b7ea771
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-09-21 18:23:46 +0200

    Add Series Class

--- main.py
+++ main.py
@@ -19,4 +19,20 @@ class Movie(Media):
     def __str__(self):
         return F"{self.title} ({self.release_year})"
     
+class Series(Media):
+    def __init__(self, title, release_year, genre, season, epsiode):
+        super().__init__(title, release_year, genre)
+        self.season = season
+        self.episode = epsiode
 
+    def play(self, views):
+        super().play(views)
+        self.episode += 1
+
+    def __str__(self):
+        season_str = str(self.season).zfill(2)
+        episode_str = str(self.episode).zfill(2)
+        return f"{self.title} S{season_str}E{episode_str}"
+    
+
+    
